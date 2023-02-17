e449224b9f2ace65e800ff8d2e6dfbc610be9255
nizamuddinshamrat <nshamrat7@gmail.com>
2023-02-17 13:14:15 +0600

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import 'app/config/app_config.dart';
+import 'app/config/app_themes.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      enableLog: true,
+      debugShowCheckedModeBanner: false,
+      title: AppConfig.APP_NAME,
+      defaultTransition: Transition.size,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: AppThemes.lightTheme(),
     ),
   );
 }
