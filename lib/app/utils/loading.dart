4d37be3598d8b23a4125741faaa10879bdcd1853
nizamuddinshamrat <nshamrat7@gmail.com>
2023-02-18 17:45:27 +0600

    loging view added

--- lib/app/utils/loading.dart
+++ lib/app/utils/loading.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-import '../config/app_assets.dart';
-
 Future<void> loader({String? msg}) => EasyLoading.show(
       status: msg ?? 'Please wait',
       maskType: EasyLoadingMaskType.none,
       indicator: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.asset(AppAssets.ASSET_YUMA_LOGO),
-          const Padding(
+        children: const [
+          Padding(
             padding: EdgeInsets.all(20.0),
             child:
                 Center(child: CircularProgressIndicator(color: Colors.yellow)),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 import 'package:get/get.dart';
 
@@ -15,6 +16,7 @@ void main() {
       defaultTransition: Transition.size,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      builder: EasyLoading.init(),
       theme: AppThemes.lightTheme(),
     ),
   );
