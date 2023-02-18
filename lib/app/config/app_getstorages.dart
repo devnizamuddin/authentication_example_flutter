fd8bf37f9965af87e601434e9f3a59706478b633
nizamuddinshamrat <nshamrat7@gmail.com>
2023-02-18 18:34:30 +0600

    get storage key class aded

---
+++ lib/app/config/app_getstorages.dart
@@ -0,0 +1,8 @@
+class AppGetStorages {
+  //GET STORAGE KEY
+  static const String KEY_USERNAME = 'username';
+  static const String KEY_PASSWORD = 'password';
+  static const String KEY_LOGIN_STATUS = 'login_status';
+  static const String KEY_USER_DATA = 'user_data';
+  static const String KEY_REMEMBER_PASS_STATUS = 'remember_password_status';
+}
