406c5aba0af30c35072c0ba8672231b80a7a8afc
nizamuddinshamrat <nshamrat7@gmail.com>
2023-02-18 18:36:04 +0600

    login api done

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,5 +1,14 @@
+import 'dart:convert';
+
+import 'package:authentication_example_flutter/app/models/login_response.dart';
+import 'package:authentication_example_flutter/app/service/api_communication.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../../../config/app_getstorages.dart';
+import '../../../routes/app_pages.dart';
+import '../../../utils/snackbar.dart';
 
 class LoginController extends GetxController {
   final GlobalKey<FormState> formKey = GlobalKey();
@@ -12,6 +21,8 @@ class LoginController extends GetxController {
 
   RxBool rememberPass = false.obs;
   RxBool obscurePassField = true.obs;
+  final _box = GetStorage();
+  final ApiCommunication _apiCommunication = ApiCommunication();
 
   Future<void> onObscureTap() async {
     obscurePassField.value = !obscurePassField.value;
@@ -20,10 +31,39 @@ class LoginController extends GetxController {
   Future<void> onRememberPassTap(bool? v) async {
     if (v != null) {
       rememberPass.value = v;
+      _box.write(AppGetStorages.KEY_REMEMBER_PASS_STATUS, v);
     }
   }
 
-  Future<void> signIn() async {}
+  Future<void> signIn() async {
+    FocusManager.instance.primaryFocus?.unfocus();
+    if (formKey.currentState!.validate()) {
+      saveLoginInfo();
+      final response = await _apiCommunication.doPostRequest(
+        requestData: {
+          'username': userNameController.text,
+          'password': passwordController.text
+        },
+        endPoint: 'consumer/login',
+      );
+      if (response != null) {
+        Map<String, dynamic> responseBody = response.data;
+        if (responseBody['error_code'] == null) {
+          _box.write(AppGetStorages.KEY_LOGIN_STATUS, true);
+          _box.write(AppGetStorages.KEY_USER_DATA, responseBody);
+          final loginResponse = LoginResponseModel.fromMap(responseBody);
+          Get.offAllNamed(Routes.HOME);
+        } else {
+          errorSnack(responseBody['error_description']);
+        }
+      }
+    }
+  }
+
+  saveLoginInfo() {
+    _box.write(AppGetStorages.KEY_USERNAME, userNameController.text);
+    _box.write(AppGetStorages.KEY_PASSWORD, passwordController.text);
+  }
 
   Future<void> signUp() async {}
 
@@ -31,8 +71,12 @@ class LoginController extends GetxController {
   void onInit() {
     userFnode = FocusNode();
     passFnode = FocusNode();
-    userNameController = TextEditingController();
-    passwordController = TextEditingController();
+    rememberPass.value =
+        _box.read(AppGetStorages.KEY_REMEMBER_PASS_STATUS) ?? false;
+    userNameController = TextEditingController(
+        text: rememberPass.value ? _box.read(AppGetStorages.KEY_USERNAME) : '');
+    passwordController = TextEditingController(
+        text: rememberPass.value ? _box.read(AppGetStorages.KEY_PASSWORD) : '');
     super.onInit();
   }
 
